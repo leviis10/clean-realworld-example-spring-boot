e981b9cda06f68c644105729eead2aee67db39da
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2026-03-24 22:32:40 +0700

    Initial Commit

---
+++ src/main/java/com/leviis/realworldexample/RealworldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.leviis.realworldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leviis/realworldexample/RealworldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leviis.realworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
