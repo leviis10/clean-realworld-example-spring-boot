843735cc7b11b063450547e3e5141ee7077e5f5a
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2026-04-11 14:44:08 +0700

    chore: Rename UserController to AuthController

--- src/main/java/com/leviis/realworldexample/user/adapter/inbound/http/AuthController.java
+++ src/main/java/com/leviis/realworldexample/user/adapter/inbound/http/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public final class UserController {
+public final class AuthController {
     private final RegisterUserUseCase registerUserUseCase;
     private final UserLoginUseCase userLoginUseCase;
 
